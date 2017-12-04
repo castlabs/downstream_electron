4e2f1850f6a5170ced7502474cf83b3f0b708656
Bartlomiej Obecny <obecny@interia.pl>
2017-12-04 16:28:37 +0100

    adding travis

---
+++ push-jsdoc-to-hg-pages.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_REPO_SLUG" == "castlabs/downstream_electron" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "travis_test" ]; then
+
+    echo -e "Publishing jsdoc...\n"
+
+    rm -rf gh-pages
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/castlabs/downstream_electron gh-pages > /dev/null
+    cd gh-pages
+    git rm -rf ./fonts
+    git rm -rf ./scripts
+    git rm -rf ./styles
+    git rm -rf *.html
+    cp -r ../jsdoc/ .
+    cp -r ../build/ ./build
+    git add .
+    git commit --amend -m "Latest documentation on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+    git push -fq origin gh-pages > /dev/null
+    cd ..
+    rm -rf gh-pages
+
+    echo -e "Published jsdoc to gh-pages.\n"
+
+fi
+
