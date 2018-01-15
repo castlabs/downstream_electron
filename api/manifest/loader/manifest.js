6f9dffd0403e4d6d3a1cc7936f4aebda79ff7495
Jeff Cunat <jeanfrancois.cunat@orange.com>
2018-01-15 15:04:40 +0100

    manage UTF-8 and UTF-16 manifest encoding

--- api/manifest/loader/manifest.js
+++ api/manifest/loader/manifest.js
@@ -17,6 +17,55 @@ const Manifest = (function () {
     }
   }
 
+  Manifest.prototype.isUTF16 = function(data) {
+    var i = 0;
+    var len = data && data.length;
+    var pos = null;
+    var b1, b2, next, prev;
+
+    if (len < 2) {
+        if (data[0] > 0xFF) {
+            return false;
+        }
+    } else {
+        b1 = data[0];
+        b2 = data[1];
+        if (b1 === 0xFF && // BOM (little-endian)
+            b2 === 0xFE) {
+            return true;
+        }
+        if (b1 === 0xFE && // BOM (big-endian)
+            b2 === 0xFF) {
+            return true;
+        }
+
+        for (; i < len; i++) {
+            if (data[i] === 0x00) {
+                pos = i;
+                break;
+            } else if (data[i] > 0xFF) {
+                return false;
+            }
+        }
+
+        if (pos === null) {
+            return false; // Non ASCII
+        }
+
+        next = data[pos + 1]; // BE
+        if (next !== void 0 && next > 0x00 && next < 0x80) {
+            return true;
+        }
+
+        prev = data[pos - 1]; // LE
+        if (prev !== void 0 && prev > 0x00 && prev < 0x80) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
   Manifest.prototype.load = function (url) {
     const _this = this;
     return new Promise(function (resolve, reject) {
@@ -26,6 +75,8 @@ const Manifest = (function () {
       _this.manifest_name = pathName.substring(pathName.lastIndexOf('/') + 1, pathName.length);
       const p = manifestLoader.load(url);
       p.then(function (v) {
+        var isEncodingUTF16 = _this.isUTF16(v.response);
+        v.response = v.response.toString(isEncodingUTF16 ? 'utf16le' : 'utf-8');
         const xml = v.response;
         _this.manifestXML = new ManifestXML_1.ManifestXML(xml, function () {
           resolve();
