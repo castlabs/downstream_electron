b54ddaa4264074a65997968c7eb7c22d8f2298d1
Elad Shalom <e@devaffair.com>
2018-03-05 16:27:19 -0400

    Update create.js
    manifest id is part of the constructor

--- api/be-methods/downloads/create.js
+++ api/be-methods/downloads/create.js
@@ -4,8 +4,8 @@ const Manifest = require("../../manifest/loader/manifest").Manifest;
 const translation = require('../../translation/index');
 
 module.exports = function (api, onSuccess, onFailure, target, manifestUrl, manifestId) {
-  let manifest = new Manifest();
-  manifest.load(manifestUrl, manifestId)
+  let manifest = new Manifest(manifestId);
+  manifest.load(manifestUrl)
       .then(() => {
         api.manifestController.cacheManifest(manifest);
         onSuccess(manifest.getJsonInfo());
