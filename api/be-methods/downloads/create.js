e8c0bc49abcc1c634de3449a1ca38e17b48759f3
Elad Shalom <e@devaffair.com>
2018-03-05 16:24:54 -0400

    Update create.js
    adding optional parameter for manifestId

--- api/be-methods/downloads/create.js
+++ api/be-methods/downloads/create.js
@@ -3,9 +3,9 @@
 const Manifest = require("../../manifest/loader/manifest").Manifest;
 const translation = require('../../translation/index');
 
-module.exports = function (api, onSuccess, onFailure, target, manifestUrl) {
+module.exports = function (api, onSuccess, onFailure, target, manifestUrl, manifestId) {
   let manifest = new Manifest();
-  manifest.load(manifestUrl)
+  manifest.load(manifestUrl, manifestId)
       .then(() => {
         api.manifestController.cacheManifest(manifest);
         onSuccess(manifest.getJsonInfo());
