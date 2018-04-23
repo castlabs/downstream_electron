80d381b02deb39a2f13bcc13019d06ecea88eee7
Jeremie COLLET <jeremie.collet@orange.com>
2018-04-23 17:29:06 +0200

    Fix a regression from commit bf2c59ae9831081817481cbee7c12a444186ef5a
    If a persistent session has been created before downloading chunks, download fails with an error : folder with manifest id already exists

--- api/be-methods/downloads/start.js
+++ api/be-methods/downloads/start.js
@@ -10,20 +10,16 @@ module.exports = function (api, onSuccess, onFailure, target, manifestId, repres
     return;
   }
 
-  canCreateManifest(manifestId).then(function () {
-    api.downloadsController.storage.getItem(manifestId).then(function (result) {
-      if (result) {
-        onFailure(translation.getError(translation.e.downloads.ALREADY_STARTED, manifestId));
-      } else {
-        api.downloadsController.start(manifestId, representations, onSuccess, function (err) {
-          onFailure(translation.getError(translation.e.downloads._GENERAL), err);
-        });
-      }
-    }, function (err) {
-      onFailure(translation.getError(translation.e.downloads._GENERAL), err);
-    });
+  api.downloadsController.storage.getItem(manifestId).then(function (result) {
+    if (result) {
+      onFailure(translation.getError(translation.e.downloads.ALREADY_STARTED, manifestId));
+    } else {
+      api.downloadsController.start(manifestId, representations, onSuccess, function (err) {
+        onFailure(translation.getError(translation.e.downloads._GENERAL), err);
+      });
+    }
   }, function (err) {
-    onFailure(translation.getError(translation.e.manifests.FOLDER_ALREADY_EXISTS, manifestId), err);
+    onFailure(translation.getError(translation.e.downloads._GENERAL), err);
   });
 
 };

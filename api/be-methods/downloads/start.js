castlabs/downstream_electron

commit bf2c59ae9831081817481cbee7c12a444186ef5a

added possibility of creating custom manifest id (#33)

api/be-methods/downloads/start.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const translation = require('../../translation/index');
+const translation = require("../../translation/index");
+const canCreateManifest = require("../../util/can-create-manifest");
 
 module.exports = function (api, onSuccess, onFailure, target, manifestId, representations) {
   const manifest = api.manifestController.getManifestById(manifestId);
@@ -8,17 +9,21 @@ module.exports = function (api, onSuccess, onFailure, target, manifestId, repres
     onFailure(translation.getError(translation.e.manifests.NOT_FOUND, manifestId));
     return;
   }
-  api.downloadsController.storage.getItem(manifestId)
-      .then(function (result) {
-        if (result) {
-          onFailure(translation.getError(translation.e.downloads.ALREADY_STARTED, manifestId));
-        } else {
-          api.downloadsController.start(manifestId, representations, onSuccess, function (err) {
-            onFailure(translation.getError(translation.e.downloads._GENERAL), err);
-          });
-        }
-      }, function (err) {
-        onFailure(translation.getError(translation.e.downloads._GENERAL), err);
-      });
+
+  canCreateManifest(manifestId).then(function () {
+    api.downloadsController.storage.getItem(manifestId).then(function (result) {
+      if (result) {
+        onFailure(translation.getError(translation.e.downloads.ALREADY_STARTED, manifestId));
+      } else {
+        api.downloadsController.start(manifestId, representations, onSuccess, function (err) {
+          onFailure(translation.getError(translation.e.downloads._GENERAL), err);
+        });
+      }
+    }, function (err) {
+      onFailure(translation.getError(translation.e.downloads._GENERAL), err);
+    });
+  }, function (err) {
+    onFailure(translation.getError(translation.e.manifests.FOLDER_ALREADY_EXISTS, manifestId), err);
+  });
 
 };

examples/main/index.js
@@ -656,8 +656,9 @@ function clearContent(name) {
 function onSubmit(e) {
   e.preventDefault();
   let value = document.getElementById('manifestUrl').value;
+  let customManifestId = document.getElementById('customManifestId').value;
   clearContent();
-  downstreamElectron.downloads.create(value).then(function (result) {
+  downstreamElectron.downloads.create(value, customManifestId).then(function (result) {
     showStatusOK('create');
     $('#contentHeader').html(result.id);
     createCheckBoxes(result.video, 'video');

api/translation/index.js
@@ -139,7 +139,7 @@ function _getUniqueKeys (groups) {
  * @private
  */
 function _parseValues (translation, values) {
-  values = values || {};
+  values = values || '';
   if (values instanceof Array) {
     const groups = _getUniqueKeys(translation.match(REGEXP_KEYS));
     for (let i = 0, j = Math.min(groups.length, values.length); i < j; i++) {

api/app-settings.js
@@ -16,6 +16,20 @@
  *   it might go to hundreds of chunks (50 files can be downloaded at the same time per manifest). Also the highger number
  *   doesn't mean it will downloads all movies faster. You should find here some balance.
  *   Seems like 2-3 manifests gives the best results, 1 manifest limitation might work better for slower computers.
+ * @property {string=} customManifestIdFolderRegex - regex to use to validate custom manifest id - bear in mind that this
+ * need to be also a valid folder name.<br>
+ * By default it matches any letter or number or unicode characters (regional characters) or "-" or "_" as a first character<br>
+ * Then it may have any letter or number or unicode characters (regional characters) or space or any of the following characters:
+ * ,.;'[]{}!@#$%&*()-_=+
+ *
+ * @property {string=} openingTagForInvalidCustomManifestIdCharacter - when customManifestId is being invalidated
+ * against customManifestIdFolderRegex it will raise the error and include the customManifestId with marked invalid characters
+ * this is the opening tag to be added before invalid character
+ *
+ * @property {regex=} closingTagForInvalidCustomManifestIdCharacter - when customManifestId is being invalidated
+ * against customManifestIdFolderRegex it will raise the error and include the customManifestId with marked invalid characters
+ * this is the closing tag to be added after invalid character
+ *
  * @namespace DownstreamElectronBE.configuration
  * @description
  * During initialization you can configure your app
@@ -98,7 +112,14 @@ let settings = {
       "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
     },
     timeout: 5000
-  }
+  },
+  // matches any letter or number or unicode characters (regional characters) or "-" or "_" as a start
+  // then it may have any letter or number or unicode characters (regional characters) or space or any of the following characters
+  // ,.;'[]{}!@#$%&*()-_=+
+  // and is all case insensitive
+  customManifestIdFolderRegex: /^([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\-|\_){1,1}([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\ |\,|\.|\;|\'|\[|\]|\{|\}|\!|\@|\#|\$|\%|\&|\*|\(|\)|\-|\_|\=|\+?){0,49}$/i,
+  openingTagForInvalidCustomManifestIdCharacter: '<span style=\'color:blue;\'>',
+  closingTagForInvalidCustomManifestIdCharacter: '</span>'
 };
 
 function loadUserSettings (jsonSettings) {
@@ -132,6 +153,15 @@ function loadUserSettings (jsonSettings) {
     }
     if (jsonSettings.numberOfManifestsInParallel) {
       settings.numberOfManifestsInParallel = jsonSettings.numberOfManifestsInParallel;
+    }
+    if (jsonSettings.customManifestIdFolderRegex) {
+      settings.customManifestIdFolderRegex = jsonSettings.customManifestIdFolderRegex;
+    }
+    if (jsonSettings.openingTagForInvalidCustomManifestIdCharacter) {
+      settings.openingTagForInvalidCustomManifestIdCharacter = jsonSettings.openingTagForInvalidCustomManifestIdCharacter;
+    }
+    if (jsonSettings.closingTagForInvalidCustomManifestIdCharacter) {
+      settings.closingTagForInvalidCustomManifestIdCharacter = jsonSettings.closingTagForInvalidCustomManifestIdCharacter;
     }
   }
 

api/be-methods-all.js
@@ -20,9 +20,20 @@ let downloads = {};
  * @method create
  * @memberOf DownstreamElectronFE.downloads
  * @param {string} manifestUrl - manifest url
+ * @param {string} customManifestId - custom manifest id, if empty, null or ''
+ * the id will be generated automatically.<br>
+ * The manifestId is used to also store information about movie under the same folder so if you overwrite it,
+ * it will be also used as a name for folder where movie content will be stored
+ * customManifestId will be validated against default regex
+ * {@link DownstreamElectronBE.configuration|customManifestIdFolderRegex}
  * @example
  * var url = "http://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd";
  * DownstreamElectronFE.downloads.create(url)
+ *    .then(
+ *      function onSuccess(result) {console.log("success", result);},
+ *      function onError(err) {console.log("error", err);
+ *    })
+ * DownstreamElectronFE.downloads.create(url, '<myCustomId>')
  *    .then(
  *      function onSuccess(result) {console.log("success", result);},
  *      function onError(err) {console.log("error", err);

api/be-methods/downloads/create.js
@@ -1,15 +1,55 @@
 "use strict";
 
 const Manifest = require("../../manifest/loader/manifest").Manifest;
-const translation = require('../../translation/index');
+const translation = require("../../translation/index");
+const canCreateManifest = require("../../util/can-create-manifest");
+const getInvalidDiff = require("../../util/get-invalid-diff");
+const appSettings = require("../../app-settings");
 
-module.exports = function (api, onSuccess, onFailure, target, manifestUrl) {
-  let manifest = new Manifest();
+module.exports = function (api, onSuccess, onFailure, target, manifestUrl, customManifestId) {
+  var useCustomId = true;
+
+  if (typeof customManifestId === "undefined" ||
+    customManifestId === "" ||
+    customManifestId === null) {
+    useCustomId = false;
+  }
+
+  if (useCustomId) {
+    if (typeof customManifestId !== "undefined" &&
+      typeof customManifestId !== "number" &&
+      typeof customManifestId !== "string") {
+      onFailure(translation.getError(translation.e.manifests.INVALID_ID, customManifestId));
+      return;
+    }
+    const customManifestIdFolderRegex = appSettings.getSettings().customManifestIdFolderRegex;
+
+    if (!customManifestId.match(customManifestIdFolderRegex)) {
+      const invalid = getInvalidDiff(
+        customManifestId,
+        customManifestIdFolderRegex,
+        appSettings.getSettings().openingTagForInvalidCustomManifestIdCharacter,
+        appSettings.getSettings().closingTagForInvalidCustomManifestIdCharacter);
+      onFailure(translation.getError(translation.e.manifests.INVALID_ID, invalid));
+      return;
+    }
+  }
+
+  let manifest = new Manifest(customManifestId);
   manifest.load(manifestUrl)
-      .then(() => {
-        api.manifestController.cacheManifest(manifest);
-        onSuccess(manifest.getJsonInfo());
-      }, (err) => {
-        onFailure(translation.getError(translation.e.manifests.LOADING_FAILED, manifestUrl), err);
-      });
+          .then(() => {
+            if (useCustomId) {
+              canCreateManifest(customManifestId).then(function () {
+                api.manifestController.cacheManifest(manifest);
+                onSuccess(manifest.getJsonInfo());
+              }, function (err) {
+                onFailure(translation.getError(translation.e.manifests.FOLDER_ALREADY_EXISTS, customManifestId), err);
+              });
+            } else {
+              api.manifestController.cacheManifest(manifest);
+              onSuccess(manifest.getJsonInfo());
+            }
+          }, (err) => {
+            onFailure(translation.getError(translation.e.manifests.LOADING_FAILED, manifestUrl), err);
+          });
 };

api/downloads/codes.js
@@ -14,6 +14,7 @@ const codes = {
     LOADING: 19,
     REMOVED: 20,
     INFO: 21,
+    EXISTS: 22,
     NOT_FOUND: 100,
   }
 };

api/downstream-electron-be.js
@@ -163,13 +163,13 @@ DownstreamElectronBE.prototype._getMethod = function (methodName) {
  */
 DownstreamElectronBE.prototype._onApiRequest = function (evt, data, target) {
   const promiseId = data.promiseId;
-  const argsObj = data.args;
+  const argsObj = data.args || {};
   const method = data.method;
   const windowId = data.windowId;
   target = windowId;
   let args = [];
   let i = 0;
-  while (argsObj[i]) {
+  while (argsObj.hasOwnProperty(i)) {
     args.push(argsObj[i]);
     i++;
   }
@@ -241,7 +241,7 @@ DownstreamElectronBE.prototype.getOfflinePath = function (manifestId) {
   if (offlineContentPort) {
     offlinePath += ":" + offlineContentPort;
   }
-  offlinePath += "/" + appSettings.getSettings().downloadsName + "/" + manifestId + "/";
+  offlinePath += "/" + encodeURIComponent(appSettings.getSettings().downloadsName) + "/" + encodeURIComponent(manifestId) + "/";
   return offlinePath;
 };
 

commit 80d381b02deb39a2f13bcc13019d06ecea88eee7

Fix a regression from commit bf2c59ae9831081817481cbee7c12a444186ef5a
If a persistent session has been created before downloading chunks, download fails with an error : folder with manifest id already exists

api/be-methods/downloads/start.js
@@ -10,20 +10,16 @@ module.exports = function (api, onSuccess, onFailure, target, manifestId, repres
     return;
   }
 
-  canCreateManifest(manifestId).then(function () {
-    api.downloadsController.storage.getItem(manifestId).then(function (result) {
-      if (result) {
-        onFailure(translation.getError(translation.e.downloads.ALREADY_STARTED, manifestId));
-      } else {
-        api.downloadsController.start(manifestId, representations, onSuccess, function (err) {
-          onFailure(translation.getError(translation.e.downloads._GENERAL), err);
-        });
-      }
-    }, function (err) {
-      onFailure(translation.getError(translation.e.downloads._GENERAL), err);
-    });
+  api.downloadsController.storage.getItem(manifestId).then(function (result) {
+    if (result) {
+      onFailure(translation.getError(translation.e.downloads.ALREADY_STARTED, manifestId));
+    } else {
+      api.downloadsController.start(manifestId, representations, onSuccess, function (err) {
+        onFailure(translation.getError(translation.e.downloads._GENERAL), err);
+      });
+    }
   }, function (err) {
-    onFailure(translation.getError(translation.e.manifests.FOLDER_ALREADY_EXISTS, manifestId), err);
+    onFailure(translation.getError(translation.e.downloads._GENERAL), err);
   });
 
 };

examples/main/index.js
@@ -2,8 +2,21 @@
 window.$ = window.jQuery = require('jquery');
 const { remote } = require('electron');
 
+let persistentPlugin = {
+  createPersistentSession : function () {
+    return new Promise ((resolve, reject ) => {
+      resolve('PERSISTENTID');
+    })
+  },
+  removePersistentSession : function () {
+    return new Promise ((resolve, reject ) => {
+      resolve();
+    });
+  }
+}
+
 // DEV
-const downstreamElectron = require('../../api/index').init(window);
+const downstreamElectron = require('../../api/index').init(window, persistentPlugin);
 // TESTING PRODUCTION
 // const downstreamElectron = require('../../dist/index').init(window);
 