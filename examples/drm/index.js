08709d9f5d47aa16829a4fdccac8c7a940846663
Bartłomiej Makowski <biuro@mackode.pl>
2019-01-23 08:51:40 +0100

    Monitor key session state and detect downloaded stream with persistent license

--- examples/drm/index.js
+++ examples/drm/index.js
@@ -10,7 +10,6 @@ const videoType = 'video/mp4;codecs="avc1.42c01e"';
 const audioType = 'audio/mp4;codecs="mp4a.40.2"';
 
 var manifestId;
-var license;
 var videoPath;
 var audioPath;
 var activeSession;
@@ -28,85 +27,147 @@ function _rawToArrayBuffer(raw) {
   return bytes.buffer;
 }
 
+function _keySystemConfig() {
+  var config = [{
+    initDataTypes: ['cenc'],
+    audioCapabilities: [{
+      contentType: audioType,
+      robustness: 'SW_SECURE_CRYPTO'
+    }],
+    persistentState: ['required'],
+    sessionTypes: ['persistent-license'],
+    videoCapabilities: [{
+      contentType: videoType,
+      robustness: 'SW_SECURE_CRYPTO'
+    }]
+  }];
+
+  return config;
+}
+
+function _handleKeyStatusesChange(event) {
+  event.target.keyStatuses.forEach(function(status, keyId) {
+    switch (status) {
+      case "usable":
+        console.log('SESSION USABLE');
+        break;
+      case "expired":
+        // Report an expired key.
+        console.log('SESSION EXPIRED');
+        break;
+      case "status-pending":
+        // The status is not yet known. Consider the key unusable until the status is updated.
+        console.log('SESSION PENDING');
+        break;
+      default:
+        // Do something with |keyId| and |status|.
+    }
+  })
+}
+
+function _createOrLoadMediaSession(resolve, pssh, session) {
+
+  function _handleMessage(event) {
+    console.log('message', event);
+  
+    var request = event.message;
+    var xmlhttp = new XMLHttpRequest();
+  
+    xmlhttp.keySession = event.target;
+    xmlhttp.open('POST', licenseUrl);
+  
+    xmlhttp.onload = function(e) {
+      var license = xmlhttp.response;
+      xmlhttp.keySession.update(license).catch(function (error) {
+        console.log('update() failed', error);
+      });
+      resolve(xmlhttp.keySession.sessionId);
+    }
+  
+    xmlhttp.responseType = "arraybuffer";
+    xmlhttp.send(request);
+  }
+
+  function _startSession(mediaKeys) {
+    var keySession = mediaKeys.createSession('persistent-license');
+    activeSession = keySession;
+    
+    keySession.addEventListener('message', _handleMessage, false);
+    keySession.addEventListener('keystatuseschange', _handleKeyStatusesChange, false);
+
+    if (session != null) {
+      keySession.load(session).then(
+        function(loaded) {
+          if (!loaded) {
+            console.log('No stored session with the ID ' + session + ' was found.');
+            // The application should remove its record of |sessionId|.
+            return;
+          }
+
+          console.log('loaded');
+          if (resolve) {
+            resolve(session);
+          }
+        }
+      );
+    } else {
+      return keySession.generateRequest('cenc', _base64ToArrayBuffer(pssh));
+    }
+  }
+
+  navigator.requestMediaKeySystemAccess('com.widevine.alpha', _keySystemConfig()).then(
+    function(keySystemAccess) {
+      console.log('keySystemAccess', keySystemAccess, keySystemAccess.getConfiguration().sessionTypes);
+
+      var video = document.querySelector('#videoOffline');
+      if (video.mediaKeys) {
+        return _startSession(video.mediaKeys);
+      }
+
+      var promise = keySystemAccess.createMediaKeys();
+      promise.catch(
+        console.error.bind(console, 'Unable to create MediaKeys')
+      );
+
+      promise.then(
+        function(createdMediaKeys) {
+          console.log('createdMediaKeys', createdMediaKeys);
+          
+          if (video.mediaKeys) {
+            return video.mediaKeys;
+          }
+          return video.setMediaKeys(createdMediaKeys);
+        }
+      ).catch(
+        console.error.bind(console, 'Unable to set MediaKeys')
+      );
+
+      promise.then(
+        function(createdMediaKeys) {
+          return _startSession(createdMediaKeys);
+        }
+      ).catch(
+        console.error.bind(console,
+          'Unable to create or initialize key session')
+      );
+    }
+  );
+}
+
 function UnsafeInMemoryPersistentPlugin () {
   this.createPersistentSession = function (persistentConfig) {
     console.log('create - persistent plugin', persistentConfig);
     return new Promise(function (resolve) {
-      const config = [{
-        initDataTypes: ['cenc'],
-        audioCapabilities: [{
-          contentType: audioType,
-          robustness: 'SW_SECURE_CRYPTO'
-        }],
-        persistentState: ['required'],
-        sessionTypes: ['persistent-license'],
-        videoCapabilities: [{
-          contentType: videoType,
-          robustness: 'SW_SECURE_CRYPTO'
-        }]
-      }];
-
-      function handleMessage(event) {
-        console.log('message', event);
-
-        var request = event.message;
-        var xmlhttp = new XMLHttpRequest();
-
-        xmlhttp.keySession = event.target;
-        xmlhttp.open('POST', licenseUrl);
-
-        xmlhttp.onload = function(e) {
-          license = xmlhttp.response;
-          xmlhttp.keySession.update(license).catch(function (error) {
-            console.log('update() failed', error);
-          });
-          resolve(xmlhttp.keySession.sessionId);
-        }
-
-        xmlhttp.responseType = "arraybuffer";
-        xmlhttp.send(request);
-      }
-
-      navigator.requestMediaKeySystemAccess('com.widevine.alpha', config).then(
-        function(keySystemAccess) {
-          console.log('keySystemAccess', keySystemAccess, keySystemAccess.getConfiguration().sessionTypes);
-    
-          var promise = keySystemAccess.createMediaKeys();
-          promise.catch(
-            console.error.bind(console, 'Unable to create MediaKeys')
-          );
-          promise.then(
-            function(createdMediaKeys) {
-              console.log('createdMediaKeys', createdMediaKeys);
-              var video = document.querySelector('#videoOffline');
-              if (video.mediaKeys) {
-                return;
-              }
-              return video.setMediaKeys(createdMediaKeys);
-            }
-          ).catch(
-            console.error.bind(console, 'Unable to set MediaKeys')
-          );
-    
-          promise.then(
-            function(createdMediaKeys) {
-              var keySession = createdMediaKeys.createSession('persistent-license');
-              activeSession = keySession;
-              keySession.addEventListener('message', handleMessage, false);
-              return keySession.generateRequest('cenc', _base64ToArrayBuffer(persistentConfig.pssh));
-            }
-          ).catch(
-            console.error.bind(console,
-              'Unable to create or initialize key session')
-          );
-        }
-      );
+      _createOrLoadMediaSession(function(sessionId) {
+        //activeSession.close();
+        resolve(sessionId);
+      }, persistentConfig.pssh, null);
     });
   };
 
   this.removePersistentSession = function (sessionId) {
     return new Promise(function (resolve) {
-      activeSession.close();
+      activeSession.remove();
       console.log('remove - persistent plugin, sessionId', sessionId);
       resolve();
     });
@@ -162,8 +223,17 @@ function onDownloadFinish(err, info) {
   $('#mainActions').append($('<input id="offline" style="margin-right: 5px" type="button" value="Play Offline">').on('click', function () {
     downstreamElectron.downloads.getOfflineLink(manifestId).then(function (result) {
       console.log(result);
-      var video = document.querySelector('#videoOffline');
-      video.play();
+
+      if (activeSession !== null) {
+        var video = document.querySelector('#videoOffline');
+        video.play();
+      } else {
+        _createOrLoadMediaSession(function(sessionId) {
+          var video = document.querySelector('#videoOffline');
+          video.play();
+        }, null, result.persistent);
+      }
+
     }, function (err) {
       console.log('play offline error', err);
     });
@@ -266,6 +336,28 @@ function addForm () {
 function onLoad() {
   addForm();
   document.getElementById('form').addEventListener('submit', onSubmit);
+
+  downstreamElectron.downloads.getListWithInfo().then(function (results) {
+    console.log(results);
+
+    for (let index = 0; index < results.length; ++index) {
+      let result = results[index];
+      if (result.status !== 'FINISHED') {
+        continue;
+      }
+
+      if (result.manifest.url === streamUrl) {
+        var info = result.manifestInfo;
+        info.manifest = result.manifest;
+        manifestId = info.id;
+        activeSession = null;
+        onDownloadFinish('', info);
+        return;
+      }
+    }
+  }, function (err) {
+    console.log(err);
+  });
 }
 
 $(document).ready(onLoad);
