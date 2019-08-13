a56ab5b55e724c47cd3a8e84e6f1099c5447b027
Bartłomiej Makowski <biuro@mackode.pl>
2019-08-13 16:18:51 +0200

    Redux Integration: basic react app

---
+++ integrations/redux/public/electron.js
@@ -0,0 +1,64 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+      width: 900, 
+      height: 680,
+      webPreferences: {
+        nodeIntegration: true
+      }
+  });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  if (isDev) {
+    const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
+
+    installExtension(REACT_DEVELOPER_TOOLS)
+        .then((name) => console.log(`Added Extension: ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
+
+    installExtension(REDUX_DEVTOOLS)
+        .then((name) => console.log(`Added Extension: ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
+
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  console.log('window-all-closed');
+  app.quit();
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+app.on('widevine-ready', (version, lastVersion) => {
+  if (null !== lastVersion) {
+    console.log('Widevine ' + version + ', upgraded from ' + lastVersion + ', is ready to be used!');
+  } else {
+    console.log('Widevine ' + version + ' is ready to be used!');
+  }
+});
+
+app.on('widevine-update-pending', (currentVersion, pendingVersion) => {
+  console.log('Widevine ' + currentVersion + ' is ready to be upgraded to ' + pendingVersion + '!');
+});
+
+app.on('widevine-error', (error) => {
+  console.log('Widevine installation encounterted an error: ' + error);
+  process.exit(1)
+});
