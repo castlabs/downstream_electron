ad607b55f00de515610e2c1f1aff7b28a7594f7b
Bartłomiej Makowski <biuro@mackode.pl>
2019-08-13 14:45:22 +0200

    Redux Integration: complete application workflow

--- integrations/redux/src/App.js
+++ integrations/redux/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fs from 'fs';
 
 function App() {
+  // writeFile function with filename, content and callback function
+  fs.writeFile('./newfile.txt', 'Learn Node FS module', function (err) {
+    if (err) throw err;
+    console.log('File is created successfully.');
+  }); 
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +23,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React now
         </a>
       </header>
     </div>
