c442b7b2af28a5aa3d2df90f1c8d6dab0478ee02
Bartek Makowski <biuro@mackode.pl>
2023-11-27 15:44:40 +0100

    DFE-38: Set required renderer options

--- integrations/typescript/src/index.ts
+++ integrations/typescript/src/index.ts
@@ -38,6 +38,11 @@ const createWindow = (): void => {
     width: 800,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+      nodeIntegration: true,
+      // NOTE: !WARNING! use with caution it allows app to download content
+      //                 from any URL
+      webSecurity: false,
+      contextIsolation: false
     },
   });
 
