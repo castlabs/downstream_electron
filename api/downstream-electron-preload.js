0c653f7401f0dcdc4cd56edb6325c918a1abff53
Bartek Makowski <biuro@mackode.pl>
2023-12-04 08:34:16 +0100

    DFE-42: Add preload script

---
+++ api/downstream-electron-preload.js
@@ -0,0 +1,29 @@
+const {
+  contextBridge,
+  ipcRenderer
+} = require("electron");
+
+const downstreamElectron = require("./downstream-electron-fe.js");
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld(
+  "downstreamElectronAPI", {
+  init: (window, persitance) => {
+    return downstreamElectron.init(window, persitance);
+  },
+  send: (channel, data) => {
+    // whitelist channels
+    let validChannels = ["downstreamElectronBE"];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  receive: (channel, func) => {
+    let validChannels = ["downstreamElectronFE"];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event, ...args) => func(args, event));
+    }
+  }
+}
+);
